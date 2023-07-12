ce039fee7e984c42fdf5a7d0289753a2d4e701fa
mo91dev <>
2023-07-12 13:22:22 -0400

    v5 Svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<main>
+    <header>
+        <nav>
+        <a href="/">Home</a>
+        <a href="/portfolio">Portfolio</a>
+        <a href="/about">About</a>
+        </nav>
+    </header>
+
+    <slot></slot> <!-- The content of each individual page goes here -->
+
+    <footer>
+        <p>&copy; {new Date().getFullYear()} Mora Ogunro</p>
+    </footer>
+</main>
+  
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    let name = 'Mora';
+</script>
+  
+<main>
+    <h1>Hello, I'm {name}</h1>
+    <p>Welcome to my portfolio!</p>
+    <a href="/portfolio">Check out my work</a>
+</main>
