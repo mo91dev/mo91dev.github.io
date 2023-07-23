f618735fa48c795755132e6996725124432039ed
mo91dev <>
2023-07-23 18:00:51 -0400

    under construction

---
+++ _next/static/oOoaoYj2rY2-nYsFEMIhv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-50d777cbd0da1b2f.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
